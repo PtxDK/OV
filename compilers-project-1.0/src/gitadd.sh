fa1786a974172c954933ec3fdc507950b99905e0
Archimedes of Syracuse <archimedes@syracuse.(none)>
2015-12-11 16:03:35 +0000

    luxus filer

---
+++ compilers-project-1.0/src/gitadd.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+git add Lexer.lex Interpreter.sml TypeChecker.sml Parser.grm CodeGen.sml
--- compilers-project-1.0/src/open.sh
+++ compilers-project-1.0/src/open.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-subl Lexer.lex && subl Interpreter.sml && subl Parser.grm && subl TypeChecker.sml && subl CodeGen.sml
+subl Lexer.lex && subl Interpreter.sml && subl TypeChecker.sml && subl Parser.grm && subl CodeGen.sml
